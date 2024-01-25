035bee17298c354b4df7ecb07dc9df5cf29938a8
adityaxprakash <cs1221595@cse.iitd.ac.in>
2024-01-26 02:13:07 +0530

    Changed attributes, added live data

--- app.py
+++ app.py
@@ -5,20 +5,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from jugaad_data.nse import stock_df
 from datetime import date, timedelta
 
-
 import get_stock_data as gsd
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key"  # Replace with your actual secret key
-
-def get_stock_data(symbol):
-    # This is a mockup. Replace this with your actual function to get stock data.
-    end_date = date.today()
-    start_date = end_date - timedelta(days=1) # get data for the last 30 days
-    df = stock_df(symbol=symbol, from_date=start_date, to_date=end_date)
-    req=['DATE','OPEN','HIGH','LOW','CLOSE','VOLUME','SYMBOL']
-    df=df[req]
-    return df.to_dict(orient='records')[0]
 
 # Database Configuration
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
@@ -55,6 +45,7 @@ def register():
 
         flash("Registration successful! Please login.")
         return redirect(url_for("index"))
+    return render_template("register.html")
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -77,45 +68,41 @@ def dashboard():
     if 'user_id' in session:        
         stock_symbols = ['DRREDDY','EICHERMOT','GRASIM','HCLTECH','ADANIENT','ADANIPORTS','APOLLOHOSP','ASIANPAINT','AXISBANK']  # Add all your 50 stock symbols here
         stockdata={}
+        # stock_symbols=gsd.nifty_50
         ascending_data = []
-        for symbols in stock_symbols:
-            stockdata[symbols]=get_stock_data(symbols)
-            ascending_data.append([stockdata[symbols]['CLOSE']-stockdata[symbols]['OPEN'],symbols])
-        print(stockdata)
-        ascending_data.sort()
-        descending_data = ascending_data[::-1]
-        return render_template('table.html', username=session['username'],stocks_data=stockdata,asc=ascending_data ,desc=descending_data)
-    else:
-        return redirect(url_for('index'))
+        for symbol in stock_symbols:
+            stockdata[symbol]=gsd.get_live_symbol_data(symbol)
+            ascending_data.append([stockdata[symbols]['lastPrice']-stockdata[symbols]['OPEN'],symbols])
+        # print(stockdata)
+        # top_5_gainers=gsd.get_top_gainers()
+        # top_5_losers=gsd.get_top_losers()
+        # ascending_data.sort()
+        # descending_data = ascending_data[::-1]
+        return render_template('table.html', username=session['username'],stocks_data=stockdata)
+    # else:
+    #     return redirect(url_for('index'))
     
-@app.route('/stock_data/<symbol>')
-def stock_data(symbol):
-    return jsonify(get_stock_data(symbol))
+# @app.route('/stock_data/<symbol>')
+# def stock_data(symbol):
+#     return jsonify(get_stock_data(symbol))
 
 @app.route('/<symbol>')
 def stock(symbol):
-    stockdata=get_stock_data(symbol)
-    return render_template('stockdata.html', symbol=symbol,data=stockdata)
+    livedata=gsd.get_live_symbol_data(symbol)
+    stockdata=gsd.get_symbol_data(symbol,1).iloc[0]
+    plot_div = gsd.plot_symbol(symbol, 'Closing Price')
+    return render_template('stockdata.html', symbol=symbol, livedata=livedata, historicaldata=stockdata, plot_div=plot_div)
     if "user_id" in session:
         return redirect(url_for("graph"))
         return render_template("welcome.html", username=session["username"])
     else:
         return redirect(url_for("index"))
-
-
-
-    if "user_id" in session:
-        return redirect(url_for("graph"))
-        return render_template("welcome.html", username=session["username"])
-    else:
-        return redirect(url_for("index"))
-
-
 
 @app.route("/index_graph")
 def index_graph():
     plot_div=gsd.plot_index()
     return render_template("graph.html", plot_div=plot_div)
+
 
 @app.route("/plot_compare", methods=["GET", "POST"])
 def plot_compare():
