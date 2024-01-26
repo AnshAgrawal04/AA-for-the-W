2ccf812394a9326d128a5f257a6d5c19055285af
adityaxprakash <cs1221595@cse.iitd.ac.in>
2024-01-26 14:26:49 +0530

    Cleaned up and added height,width func parameters

--- get_stock_data.py
+++ get_stock_data.py
@@ -7,8 +7,7 @@ from datetime import date, timedelta
 import plotly.graph_objects as go
 import pandas as pd
 from plotly.offline import plot
-# from nsetools import Nse
-# nse=Nse()
+
 n=NSELive()
 
 parameter_to_df_column = {
@@ -24,29 +23,18 @@ parameter_to_df_column = {
 }
 nifty_50 = list(pd.read_csv("ind_nifty50list.csv")["Symbol"])
 
-# def get_top_gainers():
-#     top_gainers=nse.get_top_gainers()
-#     top_gainers_price=[]
-#     for gainer in top_gainers:
-#         gains={}
-#         gains['symbol']=gainer['symbol']
-#         gains['price']=gainer['ltp']
-#         gains['pchange']=(gainer['ltp']/gainer['previousPrice']-1)*100
-#         top_gainers_price.append(gains)
-#     return top_gainers_price
+def get_nifty50():
+    return nifty_50
 
-# def get_top_losers():
-#     top_losers=nse.get_top_losers()[:5]
-#     top_losers_price=[]
-#     for loser in top_losers:
-#         losses={}
-#         losses['symbol']=loser['symbol']
-#         losses['price']=loser['ltp']
-#         losses['pchange']=(loser['ltp']/loser['previousPrice']-1)*100
-#         top_losers_price.append(losses)
-#     return top_losers_price
-
-
+def get_stock_compare_parameters():
+    return [
+        "Closing Price",
+        "Opening Price",
+        "High Price",
+        "Low Price",
+        "Volume",
+        "Average Price",
+    ]
 
 def get_live_symbol_data(symbol):
     query=n.stock_quote(symbol)
@@ -59,21 +47,18 @@ def get_index_data(index, days):
     df.drop_duplicates(subset=["HistoricalDate"], inplace=True)
     return df
 
-
 def get_symbol_data(symbol, days):
     end_date = date.today()
     start_date = end_date - timedelta(days=days)
     df = stock_df(symbol=symbol, from_date=start_date, to_date=end_date)
     return df
 
+# def add_trace(fig, df, sym_name, xcolumn, ycolumn, title):
+#     fig.add_trace(go.Scatter(x=df[xcolumn], y=df[ycolumn], mode="lines", name=sym_name))
+#     fig.update_layout(title=title)
+#     return fig
 
-def add_trace(fig, df, sym_name, xcolumn, ycolumn, title):
-    fig.add_trace(go.Scatter(x=df[xcolumn], y=df[ycolumn], mode="lines", name=sym_name))
-    fig.update_layout(title=title)
-    return fig
-
-
-def get_plot(df, sym_name, xcolumn, ycolumn, title):
+def get_plot(df, sym_name, xcolumn, ycolumn, title, height, width):
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=df[xcolumn], y=df[ycolumn], mode="lines", name=sym_name))
 
@@ -82,8 +67,8 @@ def get_plot(df, sym_name, xcolumn, ycolumn, title):
         xaxis_title="Date",
         yaxis_title=title,
         autosize=False,
-        width=715,
-        height=430,
+        width=width,
+        height=height,
     )
     fig.update_xaxes(
         rangeslider_visible=False,
@@ -102,65 +87,44 @@ def get_plot(df, sym_name, xcolumn, ycolumn, title):
     fig.update_yaxes(range=[min(df[ycolumn]), max(df[ycolumn])])
     return fig
 
-def get_stock_compare_parameters():
-    return [
-        "Closing Price",
-        "Opening Price",
-        "High Price",
-        "Low Price",
-        "Volume",
-        "Average Price",
-    ]
-
-def plot_symbol(symbol, parameter):
+def plot_symbol(symbol, parameter, height=500, width=1000):
     num_years = 2
     days = 365 * num_years
     ycolumn=  parameter_to_df_column[parameter]
     fig = get_plot(
-        get_symbol_data(symbol, days), symbol, 'DATE', ycolumn, parameter
+        get_symbol_data(symbol, days), symbol, 'DATE', ycolumn, parameter,height,width
     )
     plot_div = plot(
         fig, output_type="div", include_plotlyjs=False, config={"displayModeBar": False}
     )
     return plot_div
 
-def plot_index():
+def plot_index(height=500, width=1000):
     num_years = 2
     days = 365 * num_years
     fig = get_plot(
-        get_index_data("NIFTY 50", days), "NIFTY 50", "HistoricalDate", "CLOSE", 'Closing Prices'
+        get_index_data("NIFTY 50", days), "NIFTY 50", "HistoricalDate", "CLOSE", 'Closing Prices',height,width
     )
     plot_div = plot(
         fig, output_type="div", include_plotlyjs=False, config={"displayModeBar": False}
     )
     return plot_div
 
-def plot_and_compare_symbols(symbol_name_1, symbol_name_2, symbol_name_3, parameter):
+def plot_and_compare_symbols(symbol_name_1, symbol_name_2, symbol_name_3, parameter, height=500, width=1000):
     if symbol_name_1 not in nifty_50 or symbol_name_2 not in nifty_50:
         return None
     num_years = 2
     days = 365 * num_years
     ycol = parameter_to_df_column[parameter]
     fig = get_plot(
-        get_symbol_data(symbol_name_1, days), symbol_name_1, "DATE", ycol, parameter
+        get_symbol_data(symbol_name_1, days), symbol_name_1, "DATE", ycol, parameter,height,width
     )
-    fig = add_trace(
-        fig,
-        get_symbol_data(symbol_name_2, days),
-        symbol_name_2,
-        "DATE",
-        ycol,
-        parameter,
-    )
+    df = get_symbol_data(symbol_name_2, days)
+    fig.add_trace(go.Scatter(x=df['DATE'], y=df[ycol], mode="lines", name=symbol_name_2))
+    fig.update_layout(title=parameter)
     if symbol_name_3 in nifty_50:
-        fig = add_trace(
-            fig,
-            get_symbol_data(symbol_name_3, days),
-            symbol_name_3,
-            "DATE",
-            ycol,
-            parameter,
-        )
+        df = get_symbol_data(symbol_name_3, days)
+        fig.add_trace(go.Scatter(x=df['DATE'], y=df[ycol], mode="lines", name=symbol_name_3))
     plot_div = plot(
         fig,
         output_type="div",
@@ -169,4 +133,11 @@ def plot_and_compare_symbols(symbol_name_1, symbol_name_2, symbol_name_3, parame
     )
     return plot_div
 
-# print(get_live_symbol_data('LT'))
+# stock_symbols = ['DRREDDY','EICHERMOT','GRASIM','HCLTECH','ADANIENT','ADANIPORTS','APOLLOHOSP','ASIANPAINT','AXISBANK']  # Add all your 50 stock symbols here
+# ascending_data = []
+# stockdata={}
+# for symbol in stock_symbols:
+#     stockdata[symbol]=get_live_symbol_data(symbol)
+#     ascending_data.append({'symbol':symbol,'pchange':stockdata[symbol]['pChange'],'price':stockdata[symbol]['lastPrice']})  
+# ascending_data = sorted(ascending_data, key=lambda x: x['pchange'])
+# print(ascending_data)
