fff9b6dd35acba5acbeb79a4ee24d6525d1e3353
adityaxprakash <cs1221595@cse.iitd.ac.in>
2024-02-01 21:16:34 +0530

    Final commit

--- news.py
+++ news.py
@@ -1,17 +1,17 @@
 from newsapi import NewsApiClient
-newsapi = NewsApiClient(api_key="b313703b75dd480a99c27be963bb29cf")
-# newsapi = NewsApiClient(api_key="b5863f3db9b54e89b94560e583919438") #Alternate key
+# newsapi = NewsApiClient(api_key="b313703b75dd480a99c27be963bb29cf")
+newsapi = NewsApiClient(api_key="b5863f3db9b54e89b94560e583919438") #Alternate key
 
 top_headlines_nse = newsapi.get_top_headlines(
     q="stock market", category="business", language="en", country="in"
 )["articles"][:5]
 
 top_headlines_nse.extend(newsapi.get_top_headlines(
-    q="trading", category="business", language="en", country="in"
+    q="NIFTY", category="business", language="en", country="in"
 )["articles"][:5])
 
 top_headlines_nse.extend(newsapi.get_top_headlines(
-    q="NSE", category="business", language="en", country="in"
+    q="IPO", category="business", language="en", country="in"
 )["articles"][:5])
 
 top_headlines = list({article["title"]: article for article in top_headlines_nse}.values())
